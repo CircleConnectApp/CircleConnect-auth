1c103f4ea9dcf783f68ae93c232aeb83a3083ef6
yehiahesham2938 <s-yehia.abdelrasol@zewailcity.edu.eg>
2025-04-13 15:34:09 +0200

    Add CORS middleware to allow requests from the React development server

--- app/main.py
+++ app/main.py
@@ -7,6 +7,17 @@ import os
 app = FastAPI(title="CircleConnect Auth Service")
 
 
+origins = [
+    "http://localhost:5173",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,   
+    allow_credentials=True,
+    allow_methods=["*"],             
+    allow_headers=["*"],       
+)
 
 app.include_router(auth_router)
 
